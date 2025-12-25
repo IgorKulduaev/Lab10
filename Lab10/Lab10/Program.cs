15917d5103743f80d8d4ed6c91fb0c28066f79d3
IgorKulduaev <and.sobol39@mail.ru>
2025-12-25 19:09:04 +0200

    Add files via upload

---
+++ Lab10/Lab10/Program.cs
@@ -0,0 +1,91 @@
+﻿Console.Write("Введите часы: ");
+int hours = int.Parse(Console.ReadLine()!);
+Console.Write("Введите минуты: ");
+int minutes = int.Parse(Console.ReadLine()!);
+Console.Write("Введите секунды: ");
+int seconds = int.Parse(Console.ReadLine()!);
+Console.Write("Введите Фамилию: ");
+string surname = Console.ReadLine()!;
+Console.Write("Введите оператора: ");
+string oper = Console.ReadLine()!;
+Time vreme = new Time(hours, minutes, seconds);
+vreme.Print();
+TimeChild call = new TimeChild(hours, minutes, seconds, surname, oper);
+call.lgota();
+
+public class Time
+{
+    protected int hours;
+    protected int minutes;
+    protected int seconds;
+
+    public Time(int hours, int minutes, int seconds)
+    {
+        this.hours = hours;
+        this.minutes = minutes;
+        this.Seconds = seconds;
+    }
+    public int Hours
+    {
+        get => hours;
+        set { if (value < 0 || value > 23) { System.Environment.Exit(1); } else { this.hours = value; } }
+    }
+    public int Minutes
+    {
+        get => minutes;
+        set { if (value < 0 || value > 59) { System.Environment.Exit(1); } else { this.minutes = value; } }
+    }
+    public int Seconds
+    {
+        get => seconds;
+        set { if (value < 0 || value > 59) { return; } else { this.seconds = value; } }
+    }
+    public int TotalMinutes()
+    {
+        return hours * 60 + minutes;
+    }
+
+    public void tenMinutes()
+    {
+
+        int allMinutes = hours * 60 + minutes;
+        allMinutes -= 10;
+
+        if (allMinutes < 0)
+            allMinutes += 24 * 60;
+
+        hours = (allMinutes / 60) % 24;
+        minutes = allMinutes % 60;
+    }
+
+    public void Print()
+    {
+        Console.WriteLine($"Время: {hours:D2}:{minutes:D2}:{seconds:D2}");
+        Console.WriteLine($"Количество полных минут: {TotalMinutes()}");
+
+        tenMinutes();
+        Console.WriteLine($"Уменьшение на 10 мин: {hours:D2}:{minutes:D2}:{seconds:D2}");
+
+    }
+}
+    class TimeChild : Time
+    {
+        private string surname;
+        private string oper;
+
+        public TimeChild(int hours, int minutes, int seconds, string _surname, string _oper)
+            : base(hours, minutes, seconds)
+        {
+            this.surname = _surname;
+            this.oper = _oper;
+        }
+
+        public void lgota()
+        {
+            // Теперь поля hours доступны, так как они protected в Time
+            if (hours < 8)
+                Console.WriteLine("Ваше время является льготным");
+            else
+                Console.WriteLine("Ваше время НЕ является льготным");
+        }
+    }
